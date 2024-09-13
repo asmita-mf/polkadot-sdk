0b01a0ef1d6ba80b33e53f79d6eb1e92c69e5491
github-action-benchmark <github@users.noreply.github.com>
2024-09-13 11:54:28 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for b21da7480899fcfd5f9b8b6f44d54adb118badbb

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726218631590,
+  "lastUpdate": 1726228467713,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -10335,6 +10335,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.04458728721799995,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cyrill@parity.io",
+            "name": "Cyrill Leutwiler",
+            "username": "xermicus"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b21da7480899fcfd5f9b8b6f44d54adb118badbb",
+          "message": "[pallet-revive] uapi: allow create1 equivalent calls (#5701)\n\nThe salt argument should be optional to allow create1 equivalent calls.\n\n---------\n\nSigned-off-by: xermicus <cyrill@parity.io>",
+          "timestamp": "2024-09-13T10:33:51Z",
+          "tree_id": "2f77ea7907e318e1454ac20ae20239a581bd9f33",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/b21da7480899fcfd5f9b8b6f44d54adb118badbb"
+        },
+        "date": 1726228438377,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 127.92399999999994,
+            "unit": "KiB"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.036948341914,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.04466625319799991,
             "unit": "seconds"
           }
         ]
