3c6066f2eb56fca84a72c35c695c9aaaa90bb6b4
github-action-benchmark <github@users.noreply.github.com>
2024-09-16 15:21:44 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 1e25ada05b51dd81219e1dbc1d32da823b3b66b2

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726493162150,
+  "lastUpdate": 1726500103702,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -10643,6 +10643,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.044986660311999965,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "evgeny@parity.io",
+            "name": "Evgeny Snitko",
+            "username": "AndWeHaveAPlan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "1e25ada05b51dd81219e1dbc1d32da823b3b66b2",
+          "message": "Preflight workflow (#5692)\n\nPreflight reusable workflow - aggregated workflow to perform all\npreliminary jobs\nIt currently includes:\n- check changed files\n- `set-image` (and runner) job\n- useful variables",
+          "timestamp": "2024-09-16T14:03:53Z",
+          "tree_id": "7ccb24319c8bab5e023d32d317c9c471753bd683",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/1e25ada05b51dd81219e1dbc1d32da823b3b66b2"
+        },
+        "date": 1726500074560,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 127.93399999999993,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.044932686895999954,
+            "unit": "seconds"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.03719138983600001,
             "unit": "seconds"
           }
         ]
