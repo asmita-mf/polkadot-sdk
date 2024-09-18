27841fb81f61c230026a4b811f8034ec3ba4414a
github-action-benchmark <github@users.noreply.github.com>
2024-09-18 16:12:24 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 310ef5ce1086affdc522c4d1736211de2a7dd99e

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726668543583,
+  "lastUpdate": 1726675944003,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -25621,6 +25621,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.009143779513333403,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alex.theissen@me.com",
+            "name": "Alexander Theißen",
+            "username": "athei"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "310ef5ce1086affdc522c4d1736211de2a7dd99e",
+          "message": "revive: Limit the amount of static memory a contract can use (#5726)\n\nThis will make sure that when uploading new code that the declared\nstatic memory fits within a defined limit. We apply different limits to\ncode and data. Reason is that code will consume much more memory per\nbyte once decoded during lazy execution.\n\nThis PR:\n\n1) Remove the MaxCodeLen from the `Config` to we maintain tight control\nover it.\n2) Defines a single `STATIC_MEMORY_BYTES` knob that limits the maximum\ndecoded size.\n3) Enforces them only on upload but not on execution so we can raise\nthem later.\n4) Adapt the worst case calculation in `integrity_check`.\n5) Bumps the max stack depth from 5 to 10 as this will still fit within\nour memory envelope.\n6) The memory limit per contract is now a cool 1MB that can be spent on\ndata or code.\n7) Bump PolkaVM for good measure\n8) The blob is limited to 256kb which is just a sanity check to not even\ntry parsing very big inputs.\n\n---------\n\nCo-authored-by: Cyrill Leutwiler <cyrill@parity.io>",
+          "timestamp": "2024-09-18T14:48:30Z",
+          "tree_id": "f4149a98b6360dd41854d8da570e3a990b95e369",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/310ef5ce1086affdc522c4d1736211de2a7dd99e"
+        },
+        "date": 1726675914358,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.008912869006666765,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.021396673919999998,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.016456614553333332,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.17945552133999987,
             "unit": "seconds"
           }
         ]
