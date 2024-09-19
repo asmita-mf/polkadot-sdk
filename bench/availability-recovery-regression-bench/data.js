3f1d3cf0ca842823ccdd94fa7da91672daaa2461
github-action-benchmark <github@users.noreply.github.com>
2024-09-19 18:26:28 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for c8d5e5a383c01ee02c3cc49fbd5e07540b6b79cc

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726766433761,
+  "lastUpdate": 1726770388208,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -20671,6 +20671,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "availability-recovery",
             "value": 11.7500938733,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "14218860+iulianbarbu@users.noreply.github.com",
+            "name": "Iulian Barbu",
+            "username": "iulianbarbu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c8d5e5a383c01ee02c3cc49fbd5e07540b6b79cc",
+          "message": "cumulus/minimal-node: added prometheus metrics for the RPC client (#5572)\n\n# Description\n\nWhen we start a node with connections to external RPC servers (as a\nminimal node), we lack metrics around how many individual calls we're\ndoing to the remote RPC servers and their duration. This PR adds metrics\nthat measure durations of each RPC call made by the minimal nodes, and\nimplicitly how many calls there are.\n\nCloses #5409 \nCloses #5689\n\n## Integration\n\nNode operators should be able to track minimal node metrics and decide\nappropriate actions according to how the metrics are interpreted/felt.\nThe added metrics can be observed by curl'ing the prometheus metrics\nendpoint for the ~relaychain~ parachain (it was changed based on the\nreview). The metrics are represented by\n~`polkadot_parachain_relay_chain_rpc_interface`~\n`relay_chain_rpc_interface` namespace (I realized lining up\n`parachain_relay_chain` in the same metric might be confusing :).\nExcerpt from the curl:\n\n```\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"0.001\"} 15\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"0.004\"} 23\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"0.016\"} 23\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"0.064\"} 23\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"0.256\"} 24\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"1.024\"} 24\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"4.096\"} 24\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"16.384\"} 24\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"65.536\"} 24\nrelay_chain_rpc_interface_bucket{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\",le=\"+Inf\"} 24\nrelay_chain_rpc_interface_sum{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\"} 0.11719075\nrelay_chain_rpc_interface_count{method=\"chain_getBlockHash\",chain=\"rococo_local_testnet\"} 24\n```\n\n## Review Notes\n\nThe way we measure durations/hits is based on `HistogramVec` struct\nwhich allows us to collect timings for each RPC client method called\nfrom the minimal node., It can be extended to measure the RPCs against\nother dimensions too (status codes, response sizes, etc). The timing\nmeasuring is done at the level of the `relay-chain-rpc-interface`, in\nthe `RelayChainRpcClient` struct's method 'request_tracing'. A single\nentry point for all RPC requests done through the\nrelay-chain-rpc-interface. The requests durations will fall under\nexponential buckets described by start `0.001`, factor `4` and count\n`9`.\n\n---------\n\nSigned-off-by: Iulian Barbu <iulian.barbu@parity.io>",
+          "timestamp": "2024-09-19T17:09:51Z",
+          "tree_id": "351b215fa2a8178d624645b2dbc867e4a8d41efb",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/c8d5e5a383c01ee02c3cc49fbd5e07540b6b79cc"
+        },
+        "date": 1726770360560,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.640455937233336,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.22934129146666665,
             "unit": "seconds"
           }
         ]
