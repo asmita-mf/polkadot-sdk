c7481e21970e671fcf04ec34b5d3283556c3c384
github-action-benchmark <github@users.noreply.github.com>
2024-09-22 23:45:21 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 2e4e5bf2fd0ae19fa38951c7e5f495dd1453b2bb

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727043558587,
+  "lastUpdate": 1727048720543,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -20979,6 +20979,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "availability-recovery",
             "value": 11.524756488799998,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "oliver.tale-yazdi@parity.io",
+            "name": "Oliver Tale-Yazdi",
+            "username": "ggwpez"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2e4e5bf2fd0ae19fa38951c7e5f495dd1453b2bb",
+          "message": "[benchmarking] Reset to genesis storage after each run (#5655)\n\nThe genesis state is currently partially provided via\n`OverlayedChanges`, but these changes are reset by the runtime after the\nfirst repetition, causing the second repitition to use an invalid\ngenesis state.\n\nChanges:\n- Provide the genesis state as a `Storage` without any\n`OverlayedChanges` to make it work correctly with repetitions.\n- Add `--genesis-builder-preset` option to use different genesis preset\nnames.\n- Improve error messages.\n\n---------\n\nSigned-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>\nCo-authored-by: ggwpez <ggwpez@users.noreply.github.com>\nCo-authored-by: Bastian Köcher <git@kchr.de>\nCo-authored-by: Branislav Kontur <bkontur@gmail.com>",
+          "timestamp": "2024-09-22T22:28:38Z",
+          "tree_id": "2dad110eb0c80dd13de3dbe708759b10001a9578",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/2e4e5bf2fd0ae19fa38951c7e5f495dd1453b2bb"
+        },
+        "date": 1727048692294,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.563565274866665,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.2365429572333333,
             "unit": "seconds"
           }
         ]
