e719fe54896f5aff307c07c309cd2470c856bbab
paritytech-ci <devops-team@parity.io>
2024-09-22 17:27:10 +0000

    ___Updated docs for master___

--- master/polkadot_parachain/chain_spec/collectives/sidebar-items.js
+++ master/polkadot_parachain/chain_spec/collectives/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["COLLECTIVES_WESTEND_ED"],"fn":["collectives_westend_development_config","collectives_westend_genesis","collectives_westend_local_config","collectives_westend_session_keys"]};
+window.SIDEBAR_ITEMS = {"fn":["collectives_westend_development_config","collectives_westend_local_config"]};
