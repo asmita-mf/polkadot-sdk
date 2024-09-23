ca924d431fddfce64e73c1c9d607e47c880880b8
github-action-benchmark <github@users.noreply.github.com>
2024-09-23 18:47:29 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 71c768a9e1a467c629adc68423e47e37c855cd77

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727104679753,
+  "lastUpdate": 1727117248318,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -18916,6 +18916,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "approval-voting",
             "value": 2.722005204790043,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cyrill@parity.io",
+            "name": "Cyrill Leutwiler",
+            "username": "xermicus"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "71c768a9e1a467c629adc68423e47e37c855cd77",
+          "message": "[pallet-revive] Add chain ID to config an runtime API (#5807)\n\nThis PR adds the EVM chain ID to Config as well as a corresponding\nruntime API so contracts can query it.\n\nRelated issue: https://github.com/paritytech/revive/issues/44\n\n---------\n\nSigned-off-by: xermicus <cyrill@parity.io>\nCo-authored-by: command-bot <>",
+          "timestamp": "2024-09-23T17:26:13Z",
+          "tree_id": "10cb01fcf83462f4e4ff3ac9c6c16150e5302979",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/71c768a9e1a467c629adc68423e47e37c855cd77"
+        },
+        "date": 1727117219063,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 52943.8,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 63805.86,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.1202490206335995,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 11.654103047300005,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 2.6876286333099846,
             "unit": "seconds"
           }
         ]
