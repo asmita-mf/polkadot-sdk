186bc30b0cfc2f15c3864141501326316c7011be
github-action-benchmark <github@users.noreply.github.com>
2024-09-23 18:46:16 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 71c768a9e1a467c629adc68423e47e37c855cd77

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727104609916,
+  "lastUpdate": 1727117175982,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -21155,6 +21155,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "availability-recovery",
             "value": 11.653904912366666,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cyrill@parity.io",
+            "name": "Cyrill Leutwiler",
+            "username": "xermicus"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "71c768a9e1a467c629adc68423e47e37c855cd77",
+          "message": "[pallet-revive] Add chain ID to config an runtime API (#5807)\n\nThis PR adds the EVM chain ID to Config as well as a corresponding\nruntime API so contracts can query it.\n\nRelated issue: https://github.com/paritytech/revive/issues/44\n\n---------\n\nSigned-off-by: xermicus <cyrill@parity.io>\nCo-authored-by: command-bot <>",
+          "timestamp": "2024-09-23T17:26:13Z",
+          "tree_id": "10cb01fcf83462f4e4ff3ac9c6c16150e5302979",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/71c768a9e1a467c629adc68423e47e37c855cd77"
+        },
+        "date": 1727117146695,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.23509748819999995,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.631794673300002,
             "unit": "seconds"
           }
         ]
