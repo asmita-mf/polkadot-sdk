d25f005a1b71d894ea9c1f308f43c24e77dcedfd
paritytech-ci <devops-team@parity.io>
2024-09-24 13:43:02 +0000

    ___Updated docs for master___

--- master/snowbridge_pallet_ethereum_client/pallet/storage_types/sidebar-items.js
+++ master/snowbridge_pallet_ethereum_client/pallet/storage_types/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["CurrentSyncCommittee","FinalizedBeaconState","FinalizedBeaconStateIndex","FinalizedBeaconStateMapping","InitialCheckpointRoot","LatestFinalizedBlockRoot","NextSyncCommittee","OperatingMode","ValidatorsRoot"]};
+window.SIDEBAR_ITEMS = {"struct":["CurrentSyncCommittee","FinalizedBeaconState","FinalizedBeaconStateIndex","FinalizedBeaconStateMapping","InitialCheckpointRoot","LatestFinalizedBlockRoot","LatestSyncCommitteeUpdatePeriod","NextSyncCommittee","OperatingMode","ValidatorsRoot"]};
